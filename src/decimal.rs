f6773a9f91a371f510105c55c731769e1ccf9eac
timkoopmans <tim.koops@gmail.com>
2022-08-14 17:10:53 +1000

    store u128 by default and overflow into U256 for big ops

--- src/decimal.rs
+++ src/decimal.rs
@@ -1,5 +1,6 @@
 use checked_decimal_macro::*;
+use checked_decimal_macro::U256;
 
 #[decimal(12)]
 #[derive(Default, PartialEq, Debug, Clone, Copy)]
-pub(crate) struct FixedPoint(u128);
+pub(crate) struct FixedPoint(u128, U256);
