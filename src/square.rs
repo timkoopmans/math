a1dc498887fc585a359bc2300aa325d3d0ed784c
timkoopmans <tim.koops@gmail.com>
2022-08-14 17:11:32 +1000

    add a square function

--- src/square.rs
+++ src/square.rs
@@ -6,3 +6,26 @@ impl FixedPoint {
         self.big_mul(self)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::decimal::FixedPoint;
+    use checked_decimal_macro::*;
+
+    #[test]
+    fn test_square() {
+        let decimal = FixedPoint::from_scale(15, 1); // 1.5
+
+        let actual = decimal.square();
+        let expected = FixedPoint::new(2250000000000); // 2.25
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_big_square() {
+        let decimal = FixedPoint::new(u64::MAX.into());
+        let actual = decimal.square();
+        let expected = FixedPoint::new(340282366920938463426481119);
+        assert_eq!(actual, expected);
+    }
+}
